a1e647d7113f15058e822d6b237a7d513e506786
Zainabbbbbbb <AHM20167@spengergasse.at>
2023-05-11 11:12:46 +0200

    Fertige Willkommens Seite

--- src/main/java/at/spengergasse/med_px/MedPxApplication.java
+++ src/main/java/at/spengergasse/med_px/MedPxApplication.java
@@ -12,9 +12,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *  */
 @SpringBootApplication
 public class MedPxApplication {
-
+// #0d4db4 & #ffdcba
     public static void main(String[] args) {
         SpringApplication.run(MedPxApplication.class, args);
     }
-// aufrufen nur mit localhost:8080 und dann anmelden mit admin admin oder both both
+//width="500" height="500" align="left" vspace="10" hspace="20" aufrufen nur mit localhost:8080 und dann anmelden mit admin admin oder both both   width="300" height="300" vertical-align="middle"
 }
